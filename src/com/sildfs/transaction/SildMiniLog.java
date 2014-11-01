7087212db08ed943ef8fe1ca70ab35360086dc51
Silvery Fu <silveryfu@gmail.com>
2014-11-01 10:40:14 -0700

    ..

---
+++ src/com/sildfs/transaction/SildMiniLog.java
@@ -0,0 +1,20 @@
+package com.sildfs.transaction;
+
+/**
+ * Defines a "mini" log for one transaction; a commited mini log will become 
+ * an entry in SildLog 
+ * 
+ * @author: dif
+ */
+
+public class SildMiniLog {
+	
+	/* Append to the minilog on disk */
+	public void writeLog() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
